584740e61be80dfb65666f32625b6689acb34661
Aisha <rashaadeola@gmail.com>
2024-08-26 12:34:10 +0100

    i updated my landing page

---
+++ JS/login.js
@@ -0,0 +1,52 @@
+const userData = [
+  {
+    id: 1,
+    email: "rashaadeola@gmail.com",
+    password: "rasha",
+  },
+  {
+    id: 2,
+    email: "busariroqeeb@gmail.com",
+    password: "aisha",
+  },
+  {
+    id: 3,
+    email: "aishaadeola@gmail.com",
+    password: "adeola",
+  },
+  {
+    id: 4,
+    email: "aishaaddeola@gmail.com",
+    password: "adeola",
+  },
+  {
+    id: 5,
+    email: "aishaadeodla@gmail.com",
+    password: "adeola",
+  },
+  {
+    id: 6,
+    email: "aidshaadeola@gmail.com",
+    password: "adeola",
+  },
+  {
+    id: 7,
+    email: "aishaaddeola@gmail.com",
+    password: "adeola",
+  },
+];
+
+for(let i=0; i < 7; i++){
+
+  console.log(userData[i])
+
+}
+
+// console.log(userData[0].email)
+// console.log(userData[2].email)
+// console.log(userData[3].email)
+// console.log(userData[4].email)
+// console.log(userData[5].email)
+// console.log(userData[6].email)
+
+
---
+++ JS/test.js
@@ -0,0 +1,1 @@
+console.log("Aisha is a person");
